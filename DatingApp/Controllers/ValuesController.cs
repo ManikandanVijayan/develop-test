286e18994a08fd14f781984e8203f5d460e846cd
USER <USER@DESKTOP-RMPNAI3>
2020-12-22 21:35:24 +0530

    test

---
+++ DatingApp/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using DatingApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public DataContext _content;
+        public ValuesController(DataContext context)
+        {
+            _content = context;
+        }
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = _content.Values.ToList();
+            return Ok(data);
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var data = _content.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(data);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
